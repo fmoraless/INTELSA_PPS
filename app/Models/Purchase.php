7871ac24ef620abb2e6ff485fcd5c25b619f0a8d
Francisco Morales Sánchez <franticzero@gmail.com>
2022-04-17 17:39:26 -0400

    fillable for Purchase model

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -9,6 +9,15 @@ class Purchase extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'provider_id',
+        'user_id',
+        'purchase_date',
+        'tax',
+        'total',
+        'status',
+        'picture',
+    ];
 
     public function provider(){
         return $this->belongsTo(Provider::class);
