fedd669077c4a95dc7438ba39e58d88145d2b5c1
Francisco Morales Sánchez <franticzero@gmail.com>
2022-04-17 17:40:13 -0400

    pending task, seeders creation

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ProviderSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,6 +14,6 @@ class ProviderSeeder extends Seeder
      */
     public function run()
     {
-        //TODO: crear el seeder para proveedores
+        //TODO: crear el seeder para productos
     }
 }
