492dbe483a04ab89c702252034a5870e724fd6e3
Francisco Morales Sánchez <franticzero@gmail.com>
2022-04-17 17:40:29 -0400

    pending task, facories creation

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Provider>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class ProviderFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * Define the model's default state.
